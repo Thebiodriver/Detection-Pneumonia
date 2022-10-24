16d2333ca84deb3380d6d8b15eb2c83b8a832823
Thebiodriver <73842386+Thebiodriver@users.noreply.github.com>
2022-10-24 13:24:18 +0200

    Update streamlit_datacamp.py

--- streamlit_datacamp.py
+++ streamlit_datacamp.py
@@ -4,6 +4,7 @@ from PIL import Image
 import tensorflow as tf
 from keras.utils import img_to_array
 from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.sidebar.image("https://www.efrei.fr/wp-content/uploads/2022/01/LOGO_EFREI-PRINT_EFREI-WEB.png")
@@ -14,23 +15,28 @@ st.sidebar.markdown("Hajar El fakharany")
 st.sidebar.markdown("Samantha Mario joy")
 st.sidebar.markdown("Jean-Dylan Thomas")
 
+
 def predict(testing_image):
     
-    model = load_model('chest_xray.h5')
-    
+    model = load_model(r"C:\Users\saman\OneDrive\Documents\EFREI\Datacamp\chest_xray.h5")
     image = Image.open(testing_image).convert('RGB')
     image = image.resize((224,224))
-    image = img_to_array(image)
-    image = image.reshape(1,224,224,3)
-    
-    image=preprocess_input(image)
-    classes=model.predict(image)
+    x = img_to_array(image)
+    x=np.expand_dims(x, axis=0)
+    image_data=preprocess_input(x)
+    classes=model.predict(image_data)
     result=int(classes[0][0])
-    
+
+
+
     if result==0:
-        return "PNEUMONIA"
+            # print("Person is Affected By PNEUMONIA")
+        return "Person has Pneumonia"
     else:
-        return "Normal"
+        return "Result is Normal"
+            # print("Result is Normal")
+
+
 
 def main():
     st.title('Pneumonia Detection')
@@ -45,8 +51,7 @@ def main():
 
         # Prediction
         if st.button('Result', help='Prediction'):
-            result = predict(image)
-            print(str(result))
+            st.success(predict(image))
 
 if __name__=='__main__':
     main()
