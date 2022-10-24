03725d3fb30584720dfe8574e77b431caf5c6a23
Thebiodriver <73842386+Thebiodriver@users.noreply.github.com>
2022-10-24 13:59:46 +0200

    Update streamlit_datacamp.py

--- streamlit_datacamp.py
+++ streamlit_datacamp.py
@@ -51,7 +51,7 @@ def main():
 
         # Prediction
         if st.button('Result', help='Prediction'):
-            predict(image)
+            print(predict(image))
 
 if __name__=='__main__':
     main()
